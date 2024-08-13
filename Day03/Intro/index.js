47522946b9b16aac3daf3442dd723344330458a1
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-08-13 13:07:12 +0530

    before lunch commit

---
+++ Day03/Intro/index.js
@@ -0,0 +1,28 @@
+// define variables
+
+var name = "Amalya";
+
+console.log(name);
+
+const age = 34;
+
+console.log(age + "\n");
+
+
+// function
+function displayDetails(){
+    console.log("As a function");
+    console.log(name);
+    console.log(age);
+}
+
+displayDetails();
+
+// function to add two numbers
+
+function addNumbers(n1,n2){
+    console.log("\nAdd n1 and n2 : " + (n1+n2));
+}
+
+addNumbers(10,5);
+

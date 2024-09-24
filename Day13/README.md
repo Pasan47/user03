e20dadbb1793c0df8efff53274aa820dccb25862
Pasan.rathnayaka01@gmail.com <Pasan.rathnayaka01@gmail.com>
2024-09-24 14:45:32 +0530

    initial commit Day13

---
+++ Day13/README.md
@@ -0,0 +1,35 @@
+# L A R A V E L
+
+### Pre installations
+
+Composer
+
+### Create Laravel project
+composer create-project laravel/laravel my-laravel-project --prefer-dist
+
+### Run 
+php artisan serve
+
+### Create a new controller
+php artisan make:controller BookController
+
+### Create a new controller
+php artisan make:model Book     
+<!-- creating as plural - Books -->
+
+### Create a new controller
+php artisan make:view AddBook  
+
+### XAMPP 
+Apache and mysql
+
+### Create tables in migrations
+php artisan make:migration create_products_table
+
+### Run migartion
+php artisan migrate
+
+### File details
+api.php  --> server development (only backend)
+
+web.php --> fullstack development
---
+++ Day13/laravel-project/app/Http/Controllers/BookController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    //? Create API
+
+
+    public function index(){
+        $books = Book::all();
+
+        return response()->json($books);
+    }
+
+    public function create(Request $request){
+        $book = new Book;
+        $book-> name = $request->name;
+        $book->details = $request->details;
+        $book->save();
+        return response()->json(["message"=>"Book Added"],201);
+    }
+
+    public function show ($id){
+        $book = Book::find($id);
+
+        if(!empty($book)){
+            return response()->json($book);
+        }else{
+            return response()->json([
+                "message"=>"book not found"
+            ],400);
+        }
+    }
+
+    public function edit (Request $request, $id){
+        $book = Book::find($id);
+
+        if(!empty($book)){
+            $book->name = $request->name;
+            $book->details = $request->details;
+            $book->save();
+            return response() ->json(["message"=>"Book Edited", "user"=> $book],201);
+
+        }else{
+            return response()->json([
+                "message"=>"book not found"
+            ],400);
+        }
+    }
+
+    public function distroy($id){
+        $book = Book::findOrFail($id);
+        $book->delete();
+        return response() ->json(["message"=>"Book Deleted"],201);
+    }
+
+}
